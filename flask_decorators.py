87b03bc4af436ead4a825cb5ff730c6956d17b3d
Aravinda89 <1513223865@qq.com>
2020-10-06 23:14:45 +0800

    Decorators Flask

---
+++ flask_decorators.py
@@ -0,0 +1,11 @@
+#set FLASK_APP=flask_decorators.py
+#export FLASK_APP=flask_decorators.py
+#flask run
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def print_fib():
+    return '<h1>Fibonacci</h1>'
